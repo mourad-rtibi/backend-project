969c4ae3977ea35026fad56485be3d1564e6d197
mourad rtibi <mouradrtibi9@gmail.com>
2023-12-13 15:14:28 +0100

    my backend project v2

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/ListingController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
